d3c1a66059110c87e41c9bb9940717a17e83027f
BishnuBhakat <bhakatbishnu8@gmail.com>
2025-07-20 17:02:46 +0530

    private routes implemented

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem('isAuthenticated'); // Set this on login/register
+  const isAuthenticated = localStorage.getItem('isAuthenticated'); // ekhane je check korchi user login kora ache kina
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
